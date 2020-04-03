3ec2e10574d172cf00aeeaca04d8f6ae186c442d
Krista Rutz <rutzkri000@gmail.com>
2020-04-03 11:26:56 -0700

    Seed first two parks as data

--- NationalParkDirectoryApi/Models/NationalParkDirectoryApiContext.cs
+++ NationalParkDirectoryApi/Models/NationalParkDirectoryApiContext.cs
@@ -10,5 +10,32 @@ namespace NationalParkDirectoryApi.Models
     }
 
     public DbSet<NationalPark> NationalParks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<NationalPark>()
+          .HasData(
+              new NationalPark
+              {
+                NationalParkId = 1,
+                Name = "Acadia National Park",
+                AlphaCode = "ACAD",
+                StateCode = "ME",
+                Region = "Northeast",
+                Url = "https://www.nps.gov/acad/index.htm",
+                IsOpen = false
+              },
+              new NationalPark
+              {
+                NationalParkId = 2,
+                Name = "National Park of American Samoa",
+                AlphaCode = "NPSA",
+                StateCode = "AS",
+                Region = "Pacific-West",
+                Url = "https://www.nps.gov/npsa/index.htm",
+                IsOpen = true
+              }
+          );
+    }
   }
 }
