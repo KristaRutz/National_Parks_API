3a10a4105298781c8f0ab0a0d79a62144f9bcfcf
Krista Rutz <rutzkri000@gmail.com>
2020-04-03 14:08:51 -0700

    Set char limits on alpha and state codes

--- NationalParkDirectoryApi/Models/NationalPark.cs
+++ NationalParkDirectoryApi/Models/NationalPark.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NationalParkDirectoryApi.Models
 {
   public class NationalPark
   {
     public int NationalParkId { get; set; }
     public string Name { get; set; }
+
+    [StringLength(4)]
     public string AlphaCode { get; set; }
+
+    [StringLength(2)]
     public string StateCode { get; set; }
     public string Region { get; set; }
     public string Url { get; set; }
